1edc7c94f42e76f7eaa30ab8b7bc4ee163efefb7
wdu <wei.g.du@gmail.com>
2023-08-22 21:56:49 -0700

    correct errors makesymlinks.sh

--- makesymlinks.bat
+++ makesymlinks.bat
@@ -5,5 +5,7 @@
 set bin_path=%userprofile%\dotfiles\bin_win
 setx PATH "%PATH%;%%bin_path%%;"
 
-del %userprofile%\AppData\local\nvim
+:: del will delete all the files in source folder
+:: rmdir only delete the symbolic link
+rmdir %userprofile%\AppData\local\nvim
 mklink -d %userprofile%\AppData\local\nvim %userprofile%\dotfiles\nvim
