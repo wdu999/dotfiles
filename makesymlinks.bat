2d0ce2d42f114fde8b100ae523cbcba84192dc3f
wdu <wei.g.du@gmail.com>
2023-08-22 22:01:12 -0700

    correct errors makesymlinks.sh

--- makesymlinks.bat
+++ makesymlinks.bat
@@ -2,7 +2,7 @@
 ::
 ::
 
-set bin_path=%userprofile%\dotfiles\bin_win
+set bin_path=%userprofile%\dotfiles\bins_win
 setx PATH "%PATH%;%bin_path%;"
 
 :: del will delete all the files in source folder
