f8febd8d29d11bb716121d9a9c0d7016b0db51db
wdu <wei.g.du@gmail.com>
2023-09-29 07:39:33 -0700

    correct errors

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -19,8 +19,10 @@ nvimtree.setup({
 		icons = {
 			glyphs = {
 				folder = {
-					arrow_closed = "", -- arrow when folder is closed
-					arrow_open = "", -- arrow when folder is open
+					-- arrow_closed = "", -- arrow when folder is closed, somehow display is wrong in gnome-terminal
+					-- arrow_open = "", -- arrow when folder is open, somehow display is wrong in gnome-terminal
+					arrow_closed = "⏵",
+					arrow_open = "⏷",
 				},
 			},
 		},
